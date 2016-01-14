14a2c12e9e97ab49824c575b368225cb5c70c341
Andy Sayler <andy.sayler@gmail.com>
2016-01-14 15:52:16 -0700

    Added utility functions

---
+++ pytutamen/util.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+
+# Andy Sayler
+# 2016
+# pytutamen Package
+# Utility Functions
+
+
+### Imports ###
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+
+import uuid
+
+from . import config
+from . import crypto
+from . import api_client
+from . import accesscontrol
+
+### Functions ###
+
+def setup_new_ac_server(name, url, conf_path=None):
+
+    # Setup Conf
+    conf = config.ClientConfig(conf_path=conf_path)
+
+    # Save Server Config
+    if conf.ac_server_configured(name):
+        old_url = conf.get_ac_server_url(name)
+        if url != old_url:
+            msg = "AC Server '{}' already configured with different URL".format(name)
+            raise Exception(msg)
+    else:
+        conf.ac_server_set_url(name, url)
+
+    # Update Defaults
+    if not conf.defaults_get_ac_server():
+        conf.defaults_set_ac_server(name)
+
+def setup_new_account(ac_server_name=None, cn="new_client_cert",
+                      email=None, country="US", state="Colorado", locality="Boulder",
+                      organization="libtutamen_client", ou="libtutamen_client_account",
+                      account_userdata=None, account_uid=None,
+                      client_userdata=None, client_uid=None,
+                      conf_path=None, path_ca=None):
+
+    # Check Args
+    if len(country) != 2:
+        raise ValueError("Country must be 2-letter code")
+
+    # Setup Conf
+    conf = config.ClientConfig(conf_path=conf_path)
+
+    # Get URL
+    if not ac_server_name:
+        ac_server_name = conf.defaults_get_ac_server()
+    ac_server_url = conf.ac_server_get_url(ac_server_name)
+
+    # Get UIDs
+    if not account_uid:
+        account_uid = conf.defaults_get_account_uid()
+        if not account_uid:
+            account_uid = uuid.uuid4()
+    if not client_uid:
+        client_uid = conf.defaults_get_client_uid()
+        if not client_uid:
+            client_uid = uuid.uuid4()
+
+    # Update Defaults
+    if not conf.defaults_get_account_uid():
+        conf.defaults_set_account_uid(account_uid)
+    if not conf.defaults_get_client_uid():
+        conf.defaults_set_client_uid(client_uid)
+
+    # Generate and Save Key
+    key_pem = crypto.gen_key()
+    conf.client_set_key(account_uid, client_uid, key_pem)
+
+    # Generate and Save CSR
+    csr_pem = crypto.gen_csr(key_pem, cn, country, state, locality, organization, ou, email)
+    conf.client_set_csr(account_uid, client_uid, csr_pem)
+
+    # Bootstrap Account and Save CRT
+    apiclient = api_client.APIClient(ac_server_url, path_ca=path_ca)
+    with apiclient:
+        bootstrap = accesscontrol.BootstrapClient(apiclient)
+        ret = bootstrap.account(account_userdata=account_userdata,
+                                account_uid=account_uid,
+                                client_userdata=client_userdata,
+                                client_uid=client_uid,
+                                client_csr=csr_pem)
+    ret_account_uid, ret_client_uid, client_crt = ret
+    conf.client_set_crt(account_uid, client_uid, ac_server_name, client_crt)
+
+    # Check and Return
+    assert account_uid == ret_account_uid
+    assert client_uid == ret_client_uid
+    return account_uid, client_uid, client_crt
