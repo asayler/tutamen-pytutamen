2e254759760feadbfd9025122d040e2673050ac0
Andy Sayler <andy.sayler@gmail.com>
2016-01-17 10:54:12 -0700

    Moved storage client class to dedicted module and reworked

---
+++ pytutamen/storage.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+
+# Andy Sayler
+# 2016
+# pytutamen Package
+# Storage Client
+
+
+### Imports ###
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+
+import uuid
+
+from . import config
+from . import base
+
+
+### Constants ###
+
+PERM_SRV_COL_CREATE = "srv-col-create"
+PERM_COL_CREATE = "col-create"
+PERM_COL_READ = "col-read"
+
+_KEY_COL = "collections"
+_KEY_COL_SEC = "secrets"
+
+
+### Exceptions ###
+
+class StorageServerConnectionException(base.ServerConnectionException):
+    pass
+
+
+### Connection Objects ###
+
+class StorageServerConnection(base.ServerConnection):
+
+    def __init__(self, storage_server_name=None, conf=None, conf_path=None, **kwargs):
+
+        # Setup Conf
+        if not conf:
+            conf = config.ClientConfig(conf_path=conf_path)
+
+        # Get Storage Server Name
+        if not storage_server_name:
+            storage_server_name = conf.defaults_get_storage_server()
+            if not storage_server_name:
+                raise(StorageServerConnectionException("Missing Storage Server Name"))
+
+        # Get Storage Server URL
+        storage_server_url = conf.storage_server_get_url(storage_server_name)
+        if not storage_server_url:
+            raise(StorageServerConnectionException("Missing Storage Server URL"))
+
+        # Call Parent
+        super().__init__(server_url=storage_server_url, server_name=storage_server_name,
+                         conf=conf, conf_path=conf_path, no_client_crt=True, **kwargs)
+
+
+### Client Objects ###
+
+class StorageClient(object):
+
+    def __init__(self, storage_connection):
+
+        # Check Args
+        if not isinstance(storage_connection, ACServerConnection):
+            msg = "'storage_connection' must of an instance of '{}'".format(StorageServerConnection)
+            raise(TypeError(msg))
+
+        # Call Parent
+        super().__init__()
+
+        # Setup Properties
+        self._storage_connection = storage_connection
+
+class CollectionsClient(StorageClient):
+
+    @property
+    def create_perm(self):
+        return PERM_SRV_COL_CREATE
+
+    def create(self, tokens, userdata=None, uid=None):
+
+        ep = "{}".format(_KEY_COL)
+
+        json_out = {}
+        if userdata:
+            json_out['userdata'] = userdata
+        if uid:
+            json_out['uid'] = str(uid)
+
+        res = self._connection.http_post(ep, json=json_out, tokens=tokens)
+        res_uid = uuid.UUID(res[_KEY_COL][0])
+        if uid:
+            assert uid == res_uid
+
+        return res_uid
+
+class SecretsClient(StorageClient):
+
+    @property
+    def create_perm(self):
+        return PERM_COL_CREATE
+
+    def create(self, tokens, col_uid, data, userdata=None, uid=None):
+
+        ep = "{}/{}/{}".format(_KEY_COL, str(col_uid), _KEY_COL_SEC)
+
+        json_out = {'data': data}
+        if userdata:
+            json_out['userdata'] = userdata
+        if uid:
+            json_out['uid'] = str(uid)
+
+        res = self._connection.http_post(ep, json=json_out, tokens=tokens)
+        res_uid = uuid.UUID(res[_KEY_COL_SEC][0])
+        if uid:
+            assert uid == res_uid
+
+        return res_uid
+
+    @property
+    def fetch_perm(self):
+        return PERM_COL_READ
+
+    def fetch(self, tokens, col_uid, key_uid):
+
+        ep = "{}/{}/{}/{}/versions/latest".format(_KEY_COL, col_uid, _KEY_COL_SEC, key_uid)
+        sec = self._connection.http_get(ep, tokens=tokens)
+        return sec
